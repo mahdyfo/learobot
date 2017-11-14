a18d060a01cc06ea300c6c7a52897bbfe4025ed9
mahdyfo <mahdyfof@gmail.com>
2017-11-14 23:54:46 +0330

    Added Train methods. findWords, getWords, saveWords, saveReply

--- app/Repositories/Train.php
+++ app/Repositories/Train.php
@@ -3,13 +3,21 @@
 namespace App\Repositories;
 
 use App\Reply;
+use App\Word;
 use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\DB;
 
 class Train
 {
-    public function getWords($input)
+    /**
+     * Extract the words from a string
+     *
+     * @param string $text Contains words separated by space
+     * @return array Array of the Words extracted from text
+     */
+    public function getWords($text)
     {
-        $words = explode(' ', $input);
+        $words = explode(' ', $text);
 
         //unique
         $words = array_unique($words);
@@ -25,9 +33,27 @@ class Train
         return $words;
     }
 
+    /**
+     * Find ids of an array of words from database
+     *
+     * @param array $words
+     * @return array Ids of words in db
+     */
+    public function findWords($words)
+    {
+        $word = new Word;
+        return $word->select('id')->whereIn('word', $words)->get()->pluck('id');
+    }
+
+    /**
+     * Save Reply text in database
+     *
+     * @param string $text reply text
+     * @return Reply Object of the inserted reply
+     */
     public function saveReply($text)
     {
-        $reply = new Reply();
+        $reply = new Reply;
 
         try {
             $reply->reply = $text;
@@ -36,5 +62,29 @@ class Train
         } catch (QueryException $e) {
             return $reply->where('reply', 'like', '%' . $text . '%')->first();
         }
+    }
+
+    /**
+     * Save Words of a text in database
+     *
+     * @param string $text contains words separated by space
+     * @return array ids of the given words from db
+     */
+    public function saveWords($text)
+    {
+        //explode words
+        $words = $this->getWords($text);
+
+        //format sql insert
+        $query = [];
+        foreach ($words as $word) {
+            $query[] = '(?)';
+        }
+
+        //insert unique words
+        DB::insert(DB::raw('INSERT IGNORE INTO words(`word`) VALUES ' . implode(',', $query)), $words);
+
+        //return words ids
+        return $this->findWords($words);
     }
 }
--- database/migrations/2017_11_14_154748_create_words_table.php
+++ database/migrations/2017_11_14_154748_create_words_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateWordsTable extends Migration
 {
@@ -16,7 +16,6 @@ class CreateWordsTable extends Migration
         Schema::create('words', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('word', 30)->unique();
-            $table->timestamps();
         });
     }
 
